785e9f3f122857548c9441151dbdc80eac4e6ff6
Dan  Pereda <dpereda@gmail.com>
2018-03-02 19:08:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,12 +1,11 @@
 var playlist ={names:'song'};
 
 function updatePlaylist(playlist,artistName, songTitle){
-  //delete playlist.artistNames
   playlist[artistName]=songTitle
   return playlist
 }
 
-function removeFromPlaylist(playlists,artistName){
+function removeFromPlaylist(playlist,artistName){
  delete playlist['artistNames']
   return playlist
 }
