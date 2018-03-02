a025c309f87ff52f1ad08d44fc56fdbe0004420b
Dan  Pereda <dpereda@gmail.com>
2018-03-02 17:57:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ var playlist ={
 };
 
 function updatePlaylist(playlists,artistName, songTitle){
-  playlist.artistNames=artistName
-  playlist.songTitles=songTitle
+  playlist['artistNames']=artistName
+  playlist['songTitles']=songTitle
 }
